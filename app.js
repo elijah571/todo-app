bcd0b6238f8c21981985ba9fd1d3cecf46e8c22a
elijah751 <elijahfx43@gmail.com>
2024-03-07 21:00:41 +0100

    added css to the elements.

--- app.js
+++ app.js
@@ -4,26 +4,29 @@ renderTodoList();
 
 function renderTodoList() {
     let todoListHtml = '';
-
-    for (let i = 0; i < todoList.length; i++) {
-
-        const todo = todoList[i];
+    todoList.forEach((todo, i) => {
+         todo = todoList[i];
         const {name, dueDate} = todo;
 
-        const html = `<p>
-            ${name}  ${dueDate}
-            <button onclick="
-                todoList.splice(${i}, 1);
-                renderTodoList();
-            ">Delete</button>
-        </p>`;
+        const html = `
+            <div>${name}</div>
+            <div>  ${dueDate}</div>
+
+            <button class="js-delete delete">Delete</button>
+        `;
         todoListHtml += html;
-    }
+    })
+    
     document.querySelector('.js-todoList').innerHTML = todoListHtml;
+    document.querySelectorAll('.js-delete')
+        .forEach((deleteButton, i) => {
+            deleteButton.addEventListener('click', () => {
+                todoList.splice(i, 1);
+                renderTodoList();
+            })
+        })
 }
 
-
-   
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
